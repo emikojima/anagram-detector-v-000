52cfda4c34e944ca8cababb79e72e6407065c335
emi kojima <emi8emi@gmail.com>
2018-02-28 19:02:28 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,26 @@
-# Your code goes here!
+require 'pry'
+class Anagram 
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word 
+  end 
+  
+  def match(list) 
+    ordered_list=[]
+    #@word into alph order
+     @word.chars.sort 
+     
+    #alph order each word in list
+    ordered_list = list.each {|w| w.chars.sort}
+    end 
+    
+    #iterate through the list to see if there is a match with @word use .select 
+    
+    if ordered_list.select{|word| word == @word.chars.sort)
+      ordered_list.select{|word| word == @word.chars.sort)
+    else
+      []
+    end
+  
+end 
