dda2b7a6c5d7b297b250c79b23c9a0b1f12df178
emi kojima <emi8emi@gmail.com>
2018-02-28 19:31:08 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,12 +7,7 @@ class Anagram
   end 
   
   def match(list) 
-   
-    
-   
-    #iterate through the list to see if there is a match with @word 
-    
    list.select{|word| word.chars.sort == @word.chars.sort}
-   
   end
+  
 end 
